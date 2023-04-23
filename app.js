a5ff3648ae9a643fe6c1895861586fdb7263a2d5
web-js-master <web.js.master@gmail.com>
2023-04-23 19:58:08 +0300

    refactor:index.html input  queryselector add menu

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.querySelector(".centered__input");//Add a new task.
 var addButton=document.querySelector(".centered__btn");//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
