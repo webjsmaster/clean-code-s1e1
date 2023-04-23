0c9aa5f0a7243b01c37ff2ea4acb1d01121f44a2
web-js-master <web.js.master@gmail.com>
2023-04-23 20:01:31 +0300

    refactor:index, app name id incomplete-tasks

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.querySelector(".centered__input");//Add a new task.
 var addButton=document.querySelector(".centered__btn");//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -136,7 +136,7 @@ var taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #incompleteTasks.
+    //Append the task list item to the #incomplete-tasks.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
